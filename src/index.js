88b355a4cb94f448145733ef8dc6ccc38de0f32b
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-10-05 13:32:17 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // <-- додано
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/hw-5-react">
+      {" "}
+      {/* <-- basename важливий для GitHub Pages */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
